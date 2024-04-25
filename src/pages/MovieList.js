21c39f13d05c7c554518a7741f69c070665c06d3
gerald <geraldbaalham@live.co.uk>
2024-04-25 16:43:54 +0100

    API fetch movie list

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import { Card } from "../components";
+
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=9c4d19e7a2d34ebd6db11621f4074e53"
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <main>
-      <div className="">MovieList</div>
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap ">
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -3,16 +3,16 @@ import { MovieList, MovieDetail, Search, PageNotFound } from "../pages";
 
 export const AllRoutes = () => {
   return (
-    <>
-        <Routes>
-            <Route path="" element={<MovieList />} />
-            <Route path="movie/:id" element={<MovieDetail />} />
-            <Route path="movies/popular" element={<MovieList />} />
-            <Route path="movies/top" element={<MovieList />} />
-            <Route path="movies/upcoming" element={<MovieList />} />
-            <Route path="search" element={<Search />} />
-            <Route path="*" element={<PageNotFound />} />
-        </Routes>
-    </>
-  )
-}
+    <div className="dark:bg-slate-800">
+      <Routes>
+        <Route path="" element={<MovieList />} />
+        <Route path="movie/:id" element={<MovieDetail />} />
+        <Route path="movies/popular" element={<MovieList />} />
+        <Route path="movies/top" element={<MovieList />} />
+        <Route path="movies/upcoming" element={<MovieList />} />
+        <Route path="search" element={<Search />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
+};
