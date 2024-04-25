a2225f10dc1f71ecfd7fa91791926fca1675d7a9
gerald <geraldbaalham@live.co.uk>
2024-04-25 08:43:26 +0100

    NavBar complete

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,9 +1,7 @@
 export const MovieList = () => {
   return (
     <main>
-      <div className="text-9xl bg-emerald-200 p-10 border-8 border-blue-700 rounded-lg m-5">
-        MovieList
-      </div>
+      <div className="">MovieList</div>
     </main>
-  )
-}
+  );
+};
